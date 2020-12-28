ad91b52c1661249d26940ea76a3cc153f86e2d66
schech1 <76683319+schech1@users.noreply.github.com>
2020-12-28 19:56:14 +0200

    Add files via upload
    Added feature to display hodled BTC in EUR or USD

--- BTCDisplay.py
+++ BTCDisplay.py
@@ -7,6 +7,7 @@ import requests
 import datetime
 
 myBuyLimit = 25595  # Adjust to desired value. Will Change "Wait" or "Buy" status
+myBTC= 2.5        # Adjust tto your hodled BTC
 
 text=PapirusTextPos(False, rotation = 180)
 
@@ -15,6 +16,7 @@ text.AddText("1", 0,5 , Id="date")
 text.AddText("2", 0,30 ,Id="usd")
 text.AddText("3", 0,50 , Id="eur")
 text.AddText("4",150,75 , Id="action")
+text.AddText("5",0,75 , Id="wallet")
 
 
 def getBTC():
@@ -26,16 +28,26 @@ def getBTC():
     Date = datetime.datetime.strptime(Date, "%Y-%m-%dT%H:%M:%S%z")
     Date = time.strftime("%d.%m. %H:%M")
     update_date=(Date)
+    
+    USD = USD.replace(',','')
+    EUR = EUR.replace(',','')
+    USD=round(float(USD),2)
+    EUR=round(float(EUR),2)
 	
+    calc_value= myBTC*float(EUR)
+    calc_value=round(calc_value,2)
+
+
     USD_price=("USD:%s" % (str(USD)))
-    EUR_price=("EUR:%s" % (str(EUR)))    
+    EUR_price=("EUR:%s" % (str(EUR)))
+    
     text.UpdateText("date", update_date)
     text.UpdateText("usd",USD_price)
     text.UpdateText("eur",EUR_price)
+    text.UpdateText("wallet",str(calc_value))
+    
 
-    USD_check = USD.replace(',','')
-
-    if float(USD_check) <= myBuyLimit:        
+    if float(USD) <= myBuyLimit:        
         text.UpdateText("action", "Buy")
     else:
         text.UpdateText("action", "Wait")
@@ -44,3 +56,4 @@ def getBTC():
     return USD,EUR,Date
 
 data=getBTC()
+
